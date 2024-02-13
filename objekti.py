8c11af211e2b2d058667236b0e26b5401c25c993
Ronalds Plostnieks <ronalds.plostnieks@gmail.com>
2024-02-13 20:34:17 +0200

    Piektdienas objekts "Cilvēks"

--- objekti.py
+++ objekti.py
@@ -1,3 +1,8 @@
+# KM - Pabeigt objekta satura ierakstīšanu dokumentā. Nodrošināt, 
+# lai programma prasa ievades datus objektiem kamēr cilvēks vēlas. 
+# Visi objekti saglabāti sarakstā. Kad visi izveidoti, 
+# visiem saraksta elementiem tiek izsaukta metode, 
+# kas izveido teksta failus katram objektam.
 class Cilveks:
     def __init__(self, vards, vecums, dzimums):
         self.age = vecums
@@ -22,39 +27,44 @@ class Cilveks:
 
     def uztaisit_spamu(self, spama_mape):
         faila_nosaukums = spama_mape + "spams" + self.name + str(self.age) + ".txt"
-        sveiki_galotne = "a"
-        laimests_galotne = "usi"
+        if self.sex == "s":
+            sveiki_galotne = "a"
+            laimests_galotne = "usi"
+        elif self.sex == "v":
+            sveiki_galotne = 's'  
+            laimests_galotne = "is"  
+        else:
+            sveiki_galotne = "i"
+            laimests_galotne = "is" 
+
+        
         laimests = self.age*35
 
-        teksts = "Sveik{}(-a/-s/-i), {}(Vārds)! Tu esi laimēj{}(-usi/-is) {}(vecums*35)€!".format(sveiki_galotne, self.name, laimests_galotne, laimests)
+        teksts = "Sveik{}, {}! Tu esi laimēj{} {}€!".format(sveiki_galotne, self.name, laimests_galotne, laimests)
 
         with open(faila_nosaukums, "w", encoding="utf-8") as spams:
             spams.write(teksts)
 
-        self.nopelnit(laimests)
+        # self.nopelnit(laimests)
         self.pastastit_par_sevi()
         
 
 
 
-persona1 = Cilveks("Marta", 32, "nenosakams")
-persona1.pastastit_par_sevi()
-persona1.dzimsanas_diena()
-persona1.pastastit_par_sevi()
-# persona1.nopelnit(30)
-persona1.pastastit_par_sevi()
-persona1.uztaisit_spamu("oop/spams/")
+# persona1 = Cilveks("Marta", 32, "nenosakams")
+# persona1.uztaisit_spamu("spam/")
 
-# turpinat = "t"
-# cilveki = []
-# while turpinat == "t":
-#     vards = input("Ievadiet cilvēka vārdu!: ")
-#     vecums = int(input("Ievadiet vecumu!: "))
-#     dzimums = input("Ievadiet dzimumu (s/v)!:")
-#     cilveki.append( Cilveks(vards, vecums, dzimums) )
-#     turpinat = input("Ja vēlies pievienot vēl vienu cilvēku, nospied 't' !")
+turpinat = "t"
+cilveki = []
+while turpinat == "t":
+    vards = input("Ievadiet cilvēka vārdu!: ")
+    vecums = int(input("Ievadiet vecumu!: "))
+    dzimums = input("Ievadiet dzimumu (s/v)!:")
+    cilveki.append( Cilveks(vards, vecums, dzimums) )
+    turpinat = input("Ja vēlies pievienot vēl vienu cilvēku, nospied 't' !")
 
-# for viens in cilveki:
+for viens in cilveki:
+    viens.uztaisit_spamu("spam/")
     
 
 
